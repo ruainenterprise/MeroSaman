584398748668c32da7ad63877a0a58787fc3d244
ruainenterprise <ruainenterprise@gmail.com>
2021-12-19 20:50:19 +0545

    Inital Setup

---
+++ src/main/java/com/dao/MeropasalApplication.java
@@ -0,0 +1,13 @@
+package com.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeropasalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeropasalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dao/MeropasalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeropasalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
